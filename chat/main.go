5ff63aa199d369c41a872bb4420b26f5e2915311
A-Takamin <takamin0609@gmail.com>
2021-05-05 23:10:33 +0900

    websocket

--- chat/main.go
+++ chat/main.go
@@ -15,21 +15,24 @@ type templateHandler struct {
 	templ    *template.Template
 }
 
-func (t *templateHandler) serveHTTP(w http.ResponseWriter, r *http.Request) {
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
 	t.templ.Execute(w, nil)
 }
 
+/**
+main関数が呼ばれたらサーバがスタートするだけ。
+"/"にアクセスすると、chat.htmlを返す。
+*/
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-			
-		`))
-	})
-
-	// サーバの開始
+	r := newRoom()
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+	// チャットルームを開始します。バックグラウンドで。
+	go r.run()
+	// webサーバの開始
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
